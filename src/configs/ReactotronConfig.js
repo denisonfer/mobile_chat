29f7d67e3af639a6bea2ed1278650f48ac6d2508
Denison Ferreira <denisonferreira@MacBook-Pro-de-Denison.local>
2021-12-06 09:14:22 -0300

    Instalações dos packages

---
+++ src/configs/ReactotronConfig.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+/* eslint-disable prefer-destructuring */
+import { NativeModules } from 'react-native';
+import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+let scriptHostname;
+
+if (__DEV__) {
+  const { scriptURL } = NativeModules.SourceCode;
+  scriptHostname = scriptURL.split('://')[1].split(':')[0];
+  console.log('scriptHostname', scriptHostname);
+
+  const tron = Reactotron.configure({ host: scriptHostname })
+    .useReactNative()
+    .connect();
+
+  console.tron = tron;
+
+  console.tron.onCustomCommand('show_asyncStorage', () => {
+    AsyncStorage.getAllKeys((err, keys) => {
+      AsyncStorage.multiGet(keys, (error, stores) => {
+        stores.map((result, i, store) => {
+          console.tron.log({ [store[i][0]]: store[i][1] });
+          return true;
+        });
+      });
+    });
+  });
+
+  tron.clear();
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
+import FlashMessage from 'react-native-flash-message';
+
 import App from './App';
 
-const Index: React.FC = () => {
+const Index: React.FC = function () {
   return <App />;
 };
 
